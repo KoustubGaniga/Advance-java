2adfe6403fbd3f5a5e48971dc34dd43c688caa22
KoustubGaniga <4AL24CS109@aiet.org.in>
2026-04-26 12:45:42 +0530

    Add files via upload

---
+++ IsNumeric.java
@@ -0,0 +1,15 @@
+package string;
+
+public class IsNumeric {
+	
+	    public static boolean isNumeric(String str) {
+	        return str.matches("\\d+");
+	    }
+
+	    public static void main(String[] args) {
+	        String str = "12345";
+	        System.out.println("Is Numeric: " + isNumeric(str));
+	    }
+	}
+
+
