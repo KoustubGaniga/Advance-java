d12e0ba1afc2d623a7975301b2923982ddceb337
KoustubGaniga <4AL24CS109@aiet.org.in>
2026-04-26 12:48:37 +0530

    Add files via upload

---
+++ Truncate.java
@@ -0,0 +1,17 @@
+package string;
+
+public class Truncate {
+	
+	    public static String truncate(String str, int length) {
+	        if (str.length() <= length)
+	            return str;
+	        return str.substring(0, length) + "...";
+	    }
+
+	    public static void main(String[] args) {
+	        String str = "Java Programming Language";
+	        System.out.println(truncate(str, 10));
+	    }
+	}
+
+
